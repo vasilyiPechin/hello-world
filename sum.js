4cebe73f677f8d310d433d0008ac160e831660b3
Nikita Smirnov <cybergamer1992@gmail.com>
2018-02-23 14:54:00 +0300

    made sum function

---
+++ sum.js
@@ -0,0 +1,11 @@
+const sumFunction = (...args) => verify(args)
+const verify = function (args) {
+  const veryfied = args.map(val => {
+    if (typeof val === 'number' && !isNaN(val)) {
+      return val
+    } else return false
+  })
+  return veryfied.reduce((zero, num) => zero + num, 0)
+}
+
+console.log(sumFunction(17, 3, 8, [], true, '18'))	// 28
